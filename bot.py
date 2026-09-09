aa258ee8cae0a941c804bbb930beef1303f84baf
shahrinakik8-design <shahrinakik8@gmail.com>
2026-09-09 21:28:48 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ CONSOLE_TOKEN = (os.getenv("ZEBRASMS_CONSOLE_TOKEN") or "").strip()
 
 TELEGRAM_BOT_TOKEN = (os.getenv("TELEGRAM_BOT_TOKEN") or "").strip()
 TELEGRAM_CHAT_ID = (os.getenv("TELEGRAM_CHAT_ID") or "").strip()
-NUMBER_BOT_URL = "@testbot_miinii_bot"
+NUMBER_BOT_URL = "https://t.me/testbot_miinii_bot"
 MAIN_CHANNEL_URL = "https://t.me/"
 
 try:
@@ -96,19 +96,18 @@ def send_telegram(text):
     "parse_mode": "HTML",
     "disable_web_page_preview": True,
     "reply_markup": {
-        "inline_keyboard": [
-            [
-                {
-                    "text": "🔢 Number Bot",
-                    "url": NUMBER_BOT_URL
-                },
-                {
-                    "text": "📢 Main Channel",
-                    "url": MAIN_CHANNEL_URL
-                }
-            ]
+    "inline_keyboard": [
+        [
+            {
+                "text": "🔢 Number Bot",
+                "url": NUMBER_BOT_URL
+            },
+            {
+                "text": "📢 Main Channel",
+                "url": MAIN_CHANNEL_URL
+            }
         ]
-    }
+    ]
 }
 
     try:
