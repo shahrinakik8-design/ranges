dbadb00d3a79194c22e246eb399aaa2abe9c86ae
shahrinakik8-design <shahrinakik8@gmail.com>
2026-09-09 21:12:46 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,6 +22,8 @@ CONSOLE_TOKEN = (os.getenv("ZEBRASMS_CONSOLE_TOKEN") or "").strip()
 
 TELEGRAM_BOT_TOKEN = (os.getenv("TELEGRAM_BOT_TOKEN") or "").strip()
 TELEGRAM_CHAT_ID = (os.getenv("TELEGRAM_CHAT_ID") or "").strip()
+NUMBER_BOT_URL = "https://t.me/@testbot_miinii_bot"
+MAIN_CHANNEL_URL = "https://t.me/"
 
 try:
     CHECK_INTERVAL = int(
@@ -89,11 +91,25 @@ def send_telegram(text):
     )
 
     payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": text,
-        "parse_mode": "HTML",
-        "disable_web_page_preview": True
+    "chat_id": TELEGRAM_CHAT_ID,
+    "text": text,
+    "parse_mode": "HTML",
+    "disable_web_page_preview": True,
+    "reply_markup": {
+        "inline_keyboard": [
+            [
+                {
+                    "text": "🔢 Number Bot",
+                    "url": NUMBER_BOT_URL
+                },
+                {
+                    "text": "📢 Main Channel",
+                    "url": MAIN_CHANNEL_URL
+                }
+            ]
+        ]
     }
+}
 
     try:
         response = requests.post(
